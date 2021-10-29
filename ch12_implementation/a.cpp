0cf42ba5f2d070677920493168c9fef3942c4522
y00njae <rladbswo12@gist.ac.kr>
2021-10-29 21:21:01 +0900

    3-12

--- ch12_implementation/a.cpp
+++ ch12_implementation/a.cpp
@@ -4,5 +4,23 @@
 using namespace std;
 
 int main() {
-  cout << -1 % 4;
-}
+  vector<int> a = {1, 2, 3};
+  vector<int> b = a;
+  b[1] = 999;
+
+  int aSize = a.size();
+  int bSize = b.size();
+  cout << "a is ";
+  for(int i=0; i<aSize; i++) {
+    cout << a[i] << " ";
+  }
+  cout << endl;
+
+  cout << "b is ";
+  for(int i=0; i<bSize; i++) {
+    cout << b[i] << " ";
+  }
+  cout << endl;
+  
+  return 0;
+} 
